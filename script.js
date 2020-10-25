e0ba9b6c60d091335010461ec92dce6bf364a301
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2020-10-25 16:37:24 +0900

    INIT

---
+++ script.js
@@ -0,0 +1,32 @@
+const URL = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
+const quote = document.querySelector('#quote');
+const quoteButton = document.querySelector('#quote-button');
+
+quoteButton.addEventListener('click', (e) => {
+  fetch(URL).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res);
+  }).then(data => {
+    console.log(data);
+    quote.innerText = data[0];
+  }).catch(err => {
+    quote.innerText = 'Unable to fetch some quotes.';
+  });
+});
+
+window.addEventListener('DOMContentLoaded', (e) => {
+  fetch(URL).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res);
+  }).then(data => {
+    console.log(data);
+    quote.innerText = data[0];
+  }).catch(err => {
+    quote.innerText = 'Unable to fetch some quotes.';
+  });
+})
+
